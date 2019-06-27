8a838ab1b12a9fe627adae3cbc5586998bdfd60f
Addison-Stavlo <AddieStavlo@gmail.com>
2019-06-27 16:51:09 -0400

    movement requests and updates added

--- mud_app/src/components/GamePage.js
+++ mud_app/src/components/GamePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login_HOC from "./Login_HOC";
-import Button from "react-bootstrap/Button";
+import { Alert, Button } from "react-bootstrap";
+
 function GamePage(props) {
   const [roomInfo, setRoomInfo] = React.useState({ players: [] });
 
@@ -17,17 +18,35 @@ function GamePage(props) {
       }
     });
     let response = await responseRaw.json();
-    console.log(responseRaw);
-    console.log(response);
     setRoomInfo(response);
   };
+
+  async function move(dir) {
+    let responseRaw = await fetch("http://localhost:8000/api/adv/move/", {
+      method: "post",
+      body: JSON.stringify({ direction: dir }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `token ${localStorage.getItem("token")}`
+      }
+    });
+    let response = await responseRaw.json();
+    console.log(response);
+    setRoomInfo(response);
+  }
 
   return (
     <>
       <h1>Room: {roomInfo.title}</h1>
       <h2>{roomInfo.description}</h2>
       <h3>Players in Room: {roomInfo.players.map(player => player)}</h3>
-      <Button onClick={initGame}>Init Game</Button>
+      {roomInfo.error_msg ? (
+        <Alert variant={"danger"}>{roomInfo.error_msg}</Alert>
+      ) : null}
+      <Button onClick={() => move("n")}>Move N</Button>
+      <Button onClick={() => move("s")}>Move S</Button>
+      <Button onClick={() => move("e")}>Move E</Button>
+      <Button onClick={() => move("w")}>Move W</Button>
     </>
   );
 }
