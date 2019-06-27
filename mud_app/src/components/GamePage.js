44837446bf654f787cda811554be7de39adbab68
Addison-Stavlo <AddieStavlo@gmail.com>
2019-06-27 16:25:31 -0400

    game init on mount of game page

--- mud_app/src/components/GamePage.js
+++ mud_app/src/components/GamePage.js
@@ -2,11 +2,32 @@ import React from "react";
 import Login_HOC from "./Login_HOC";
 import Button from "react-bootstrap/Button";
 function GamePage(props) {
+  const [roomInfo, setRoomInfo] = React.useState({ players: [] });
+
+  React.useEffect(() => {
+    initGame();
+  }, []);
+
+  const initGame = async () => {
+    let responseRaw = await fetch("http://localhost:8000/api/adv/init/", {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `token ${localStorage.getItem("token")}`
+      }
+    });
+    let response = await responseRaw.json();
+    console.log(responseRaw);
+    console.log(response);
+    setRoomInfo(response);
+  };
+
   return (
     <>
-      <h1>This is the Game Page...</h1>
-      <h2>The Game is a LIE...</h2>
-      <Button onClick={props.logOut}>Log Out</Button>
+      <h1>Room: {roomInfo.title}</h1>
+      <h2>{roomInfo.description}</h2>
+      <h3>Players in Room: {roomInfo.players.map(player => player)}</h3>
+      <Button onClick={initGame}>Init Game</Button>
     </>
   );
 }
