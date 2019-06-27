391fd209e8487b2ad02bc8f73c6e646bbb092316
Addison-Stavlo <AddieStavlo@gmail.com>
2019-06-27 15:29:28 -0400

    checks for login token on mounting

--- mud_app/src/components/Login_HOC.js
+++ mud_app/src/components/Login_HOC.js
@@ -7,14 +7,28 @@ export default function Login_HOC(Component) {
       isLoggedIn: false
     };
 
+    componentDidMount() {
+      //check for token
+      let token = localStorage.getItem("token");
+      //currently using infinite duration token, no need to check expiration
+      if (token) {
+        this.setState({ isLoggedIn: true });
+      }
+    }
+
     logIn = key => {
       this.setState({ isLoggedIn: true });
       localStorage.setItem("token", key);
     };
 
+    logOut = () => {
+      this.setState({ isLoggedIn: false });
+      localStorage.removeItem("token");
+    };
+
     render() {
       if (this.state.isLoggedIn) {
-        return <Component />;
+        return <Component logOut={this.logOut} />;
       } else {
         return <Login logIn={this.logIn} />;
       }
