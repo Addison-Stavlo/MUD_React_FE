05d0c6e2c99aaf8497ef72c97b5703062d9dc31e
Addison-Stavlo <AddieStavlo@gmail.com>
2019-06-27 18:22:16 -0400

    styled controller positions

--- mud_app/src/components/movement/MovementController.js
+++ mud_app/src/components/movement/MovementController.js
@@ -5,14 +5,44 @@ import { Alert, Button } from "react-bootstrap";
 export default function MovementController(props) {
   return (
     <ControllerBox>
-      <Button onClick={() => props.move("n")}>North</Button>
-      <Button onClick={() => props.move("s")}>South</Button>
-      <Button onClick={() => props.move("e")}>East</Button>
-      <Button onClick={() => props.move("w")}>West</Button>
+      <div className="button-holder">
+        <h3>Move</h3>
+        <Button className="n-btn" onClick={() => props.move("n")}>
+          North
+        </Button>
+        <div className="button-e-w-group">
+          <Button className="w-btn" onClick={() => props.move("w")}>
+            West
+          </Button>
+          <Button className="e-btn" onClick={() => props.move("e")}>
+            East
+          </Button>
+        </div>
+        <Button className="s-btn" onClick={() => props.move("s")}>
+          South
+        </Button>
+      </div>
 
       {props.error ? <Alert variant={"danger"}>{props.error}</Alert> : null}
     </ControllerBox>
   );
 }
 
-const ControllerBox = styled.div``;
+const ControllerBox = styled.div`
+  margin: 0 auto;
+  max-width: 250px;
+
+  .button-holder {
+    box-shadow: 0 0 10px 0 lightgray;
+    padding: 20px;
+    border-radius: 5px;
+
+    h3 {
+      margin-top: 0px;
+    }
+
+    button {
+      margin: 5px 20px;
+    }
+  }
+`;
