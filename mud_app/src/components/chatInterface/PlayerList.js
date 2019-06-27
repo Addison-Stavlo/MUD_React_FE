8ddad17c282c807ec5a728e71fc410f00a2e327c
Addison-Stavlo <AddieStavlo@gmail.com>
2019-06-27 18:32:58 -0400

    added scroll overflow for player list

--- mud_app/src/components/chatInterface/PlayerList.js
+++ mud_app/src/components/chatInterface/PlayerList.js
@@ -3,6 +3,12 @@ import ListGroup from "react-bootstrap/ListGroup";
 import styled from "styled-components";
 
 export default function PlayerList(props) {
+  // only for testing style on overflowing room population
+  //   let players = [];
+  //   for (let i = 0; i < 20; i++) {
+  //     players.push(`player${i}`);
+  //   }
+
   return (
     <PlayersList>
       <h3>Players in Room: </h3>
@@ -19,8 +25,16 @@ const PlayersList = styled.div`
   /* display: flex;
     flex-direction: column; */
   width: 300px;
-  min-height: 400px;
+  min-height: 300px;
+  max-height: 600px;
   /* border: 1px solid lightgray; */
   border-radius: 5px;
   box-shadow: 0 0 10px 0 lightgray;
+
+  .list-group {
+    max-height: 550px;
+    height: inherit;
+    overflow-y: auto;
+    margin: 10px;
+  }
 `;
